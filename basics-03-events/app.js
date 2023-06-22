7b7ddf2c531d171297df9a661fbf538936e29656
parom <E.Parominskiy@isida.by>
2023-06-22 15:45:51 +0300

    use watcher if need use more than one value in state

--- basics-03-events/app.js
+++ basics-03-events/app.js
@@ -1,12 +1,26 @@
 const app = Vue.createApp(
     {
         computed: {
-            fullname() {
-                console.log('Runing again...');
-                if (this.name === '') {
-                    return ''
+            /*  fullname() {
+                 console.log('Runing again...');
+                 if (this.name === '') {
+                     return ''
+                 }
+                 return this.name + ' ' + 'Schwarzhuller'
+             } */
+        },
+        watch: {
+            name(value) {
+                if (value === '') {
+                    this.fullName = ''
                 }
-                return this.name + ' ' + 'Schwarzhuller'
+                this.fullname = value + ' ' + this.lastName
+            },
+            lastName(value) {
+                if (value === '') {
+                    this.fullName = ''
+                }
+                this.fullname = this.name + ' ' + value
             }
         },
         data() {
@@ -14,6 +28,7 @@ const app = Vue.createApp(
                 counter: 10,
                 name: '',
                 confirmedName: '',
+                lastName: '',
             };
         },
         methods: {
@@ -35,6 +50,7 @@ const app = Vue.createApp(
             },
             resetInput() {
                 this.name = ''
+                this.lastName = ''
             },
             outputFullname() {
                 console.log('Runing again...');
