e55f8898d66838fe6ba8efa4962a54109a708812
parom <E.Parominskiy@isida.by>
2023-06-22 08:32:47 +0300

    how Vue manage html

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-Vue.createApp({
-    data() {
-      return {
-        goals: [],
-        enteredValue: ''
-      };
-    },
-    methods: {
-      addGoal() {
-        this.goals.push(this.enteredValue);
-        this.enteredValue = '';
-      }
+const app = Vue.createApp(
+    {
+        data() {
+            return {
+                courseGoal: 'Finish the course and learn Vue!'
+            };
+        },
     }
-  }).mount('#app');
+)
 
-/* const buttonEl = document.querySelector('button')
-const inputEl =  document.querySelector('input')
-const ulEl =  document.querySelector('ul')
-
-function addGoal(){
-    const enteredValue = inputEl.value
-    const itemEl = document.createElement('li')
-    itemEl.textContent = enteredValue
-    ulEl.appendChild(itemEl)
-    inputEl.value = ''
-}
-
-buttonEl.addEventListener('click', addGoal) */
+app.mount('#user-goal')
