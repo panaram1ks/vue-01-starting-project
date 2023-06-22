8d80544f3edf8fbf2ab0ae627c5d47905c74adef
parom <E.Parominskiy@isida.by>
2023-06-22 15:57:00 +0300

    use watchers

--- basics-03-events/app.js
+++ basics-03-events/app.js
@@ -10,6 +10,13 @@ const app = Vue.createApp(
             }
         },
         watch: {
+            counter(value) {
+                if (value > 50) {
+                    const that = this
+                    setTimeout(() => that.counter = 0, 2000)
+                    /* this.counter = 0 */
+                }
+            }
             /*             name(value) {
                             if (value === '') {
                                 this.fullName = ''
