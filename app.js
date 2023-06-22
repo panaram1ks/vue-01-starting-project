85abb6d50ae353ca5b34924f923ab5cda3f69352
parom <E.Parominskiy@isida.by>
2023-06-22 08:59:32 +0300

    binding data inside Vue component

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const app = Vue.createApp(
     {
         data() {
             return {
-                courseGoal: 'Finish the course and learn Vue!',
+                courseGoalA: 'Finish the course and learn Vue!',
+                courseGoalB: 'Master Vue and build amazing apps!',
                 vueLink: 'https://vuejs.org/'
             };
         },
@@ -11,9 +12,9 @@ const app = Vue.createApp(
             outputGoal: function () {
                 const randomNumber = Math.random();
                 if (randomNumber < 0.5) {
-                    return 'Learn Vue!';
+                    return this.courseGoalA;
                 } else {
-                    return 'Master Vue!';
+                    return this.courseGoalB;
                 }
             }
 
