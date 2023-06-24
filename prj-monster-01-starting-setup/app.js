d0d73ab3c9e56bee4d0d2de79edc9f1f1c9c4870
evgen <parom.evgen@mail.ru>
2023-06-24 18:07:17 +0300

    health functionality

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -12,31 +12,41 @@ const app = Vue.createApp({
   },
   methods: {
     attackMonster() {
-      this.currentRound++;
-      const attackValue = getRandomValue(12, 5);
-      this.monsterHealth -= attackValue;
-      this.attackPlayer();
+      this.currentRound++
+      const attackValue = getRandomValue(12, 5)
+      this.monsterHealth -= attackValue
+      this.attackPlayer()
     },
     attackPlayer() {
-      const attackValue = getRandomValue(12, 8);
-      this.playerHealth -= attackValue;
+      const attackValue = getRandomValue(12, 8)
+      this.playerHealth -= attackValue
     },
     spetialAttackMonster() {
-      this.currentRound++;
-      const attackValue = getRandomValue(10, 25);
-      this.monsterHealth -= attackValue;
-      this.attackPlayer();
+      this.currentRound++
+      const attackValue = getRandomValue(10, 25)
+      this.monsterHealth -= attackValue
+      this.attackPlayer()
     },
   },
   computed: {
     mayUseSpecialAttack() {
-      return this.currentRound % 3 !== 0;
+      return this.currentRound % 3 !== 0
     },
     monsterBarStyles() {
-      return { width: this.monsterHealth + "%" };
+      return { width: this.monsterHealth + "%" }
     },
     playerBarStyles() {
-      return { width: this.playerHealth + "%" };
+      return { width: this.playerHealth + "%" }
+    },
+    healPlayer() {
+      this.currentRound++
+      const healValue = getRandomValue(8, 20)
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100
+      } else {
+        this.playerHealth += healValue
+      }
+      this.attackMonster()
     },
   },
 });
