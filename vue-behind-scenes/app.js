02e18e6e13fcc087fc1a966d6ad048d22672a43e
evgen <parom.evgen@mail.ru>
2023-06-25 08:12:21 +0300

    Reactivity deep Dive

---
+++ vue-behind-scenes/app.js
@@ -0,0 +1,48 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: "",
+      message: "Vue is great!",
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+app.mount("#app");
+
+
+// ... JS is not Reactive
+// let message = "Hello";
+// let longMessage = message + "World!";
+// console.log(longMessage);
+// message = "Hello11111111111";
+// console.log(longMessage);
+
+const data = {
+  message: 'Hello',
+  longMessage: 'Hello! World!',
+}
+
+const handler = {
+  set(target, key, value){
+    // console.log(target);
+    // console.log(key);
+    // console.log(value);
+    if(key === 'message') {
+      target.longMessage = value + ' World!'
+    }
+    target.message = value
+  }
+}
+
+const proxy = new Proxy(data, handler);
+proxy.message = '1234'
+
+console.log(proxy.longMessage);
