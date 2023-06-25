ed40a62bd580a0ac706fda9308fb175aa9e7c901
evgen <parom.evgen@mail.ru>
2023-06-25 09:51:56 +0300

    oneApp vs Multiple App

--- vue-behind-scenes/app.js
+++ vue-behind-scenes/app.js
@@ -18,31 +18,39 @@ const app = Vue.createApp({
 app.mount("#app");
 
 
-// ... JS is not Reactive
-// let message = "Hello";
-// let longMessage = message + "World!";
-// console.log(longMessage);
-// message = "Hello11111111111";
-// console.log(longMessage);
-
-const data = {
-  message: 'Hello',
-  longMessage: 'Hello! World!',
-}
-
-const handler = {
-  set(target, key, value){
-    // console.log(target);
-    // console.log(key);
-    // console.log(value);
-    if(key === 'message') {
-      target.longMessage = value + ' World!'
+const app2 = Vue.createApp({
+  data(){
+    return {
+      favoriteMeal: 'Pizza'
     }
-    target.message = value
   }
-}
+});
+app2.mount("#app2");
 
-const proxy = new Proxy(data, handler);
-proxy.message = '1234'
+// // ... JS is not Reactive
+// // let message = "Hello";
+// // let longMessage = message + "World!";
+// // console.log(longMessage);
+// // message = "Hello11111111111";
+// // console.log(longMessage);
 
-console.log(proxy.longMessage);
+// const data = {
+//   message: 'Hello',
+//   longMessage: 'Hello! World!',
+// }
+
+// const handler = {
+//   set(target, key, value){
+//     // console.log(target);
+//     // console.log(key);
+//     // console.log(value);
+//     if(key === 'message') {
+//       target.longMessage = value + ' World!'
+//     }
+//     target.message = value
+//   }
+// }
+
+// const proxy = new Proxy(data, handler);
+// proxy.message = '1234'
+// console.log(proxy.longMessage);
