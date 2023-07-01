016cf4ca8ea59f953c05ce084449b480621aa646
evgen <parom.evgen@mail.ru>
2023-07-01 22:39:21 +0300

    assessment $emit custom event practice!

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <active-user :username="user.name" :userage="user.age"></active-user>
+    <user-data @set-data="setUserData"></user-data>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "Parom1 Evgen",
+        age: 35,
+      },
+    };
+  },
+  methods: {
+    setUserData(nameParameter, ageParameter) {
+      this.user = {
+        name: nameParameter,
+        age: +ageParameter,
+      };
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+</style>
---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <h2>{{ username }}</h2>
+    <h3>{{ userage }} Years</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["username", "userage"],
+};
+</script>
+
+<style>
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  border: 1px solid #ccc;
+}
+</style>
