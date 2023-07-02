1f60f8718d0837dad953feb9f49562c8594c3383
evgen <parom.evgen@mail.ru>
2023-07-02 21:11:11 +0300

    Modal dialog

--- project-01-starting/src/components/learning-resources/AddResource.vue
+++ project-01-starting/src/components/learning-resources/AddResource.vue
@@ -1,4 +1,20 @@
 <template>
+  <base-dialog
+    v-if="inputIsInvalid"
+    title="Invalid Input"
+    @close="confirmError"
+  >
+    <template v-slot:default>
+      <p>Unfortunately, at least one input value is invalid.</p>
+      <p>
+        Please check all inputs and make sure you enter at least few characters
+        into each input fields.
+      </p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError"> Okay</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -34,8 +50,26 @@ export default {
       const enteredTitle = this.$refs.titleInput.value;
       const enteredDescription = this.$refs.descInput.value;
       const enteredUrl = this.$refs.linkInput.value;
+
+      if (
+        enteredTitle.trim() === '' ||
+        enteredDescription.trim() === '' ||
+        enteredUrl.trim() === ''
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
+
       this.addResource(enteredTitle, enteredDescription, enteredUrl);
     },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
   },
 };
 </script>
