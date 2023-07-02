28f60b6b18bfc14a0ccad8c88cf61527a020b4c8
evgen <parom.evgen@mail.ru>
2023-07-02 13:04:11 +0300

    Scoped Slots

--- diving-deeper-components-01-starting-setup/src/App.vue
+++ diving-deeper-components-01-starting-setup/src/App.vue
@@ -8,13 +8,21 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
+
 export default {
   // components: {
   //   "the-header": TheHeader,
@@ -23,6 +31,7 @@ export default {
     TheHeader,
     UserInfo,
     BadgeList,
+    CourseGoals,
   },
   data() {
     return {
