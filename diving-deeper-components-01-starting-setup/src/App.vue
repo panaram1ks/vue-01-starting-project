5b75a9201ff2ee7588ec62f29e1e03c95768440c
evgen <parom.evgen@mail.ru>
2023-07-02 13:24:22 +0300

    Dinamic Components! less code switch between components

--- diving-deeper-components-01-starting-setup/src/App.vue
+++ diving-deeper-components-01-starting-setup/src/App.vue
@@ -18,8 +18,10 @@
 
     <button @click="setSelectedComponent('active-goals')">Active Goals</button>
     <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
-    <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
-    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
