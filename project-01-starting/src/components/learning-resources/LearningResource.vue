b737edbd318afb9336b2353d720cbdfc3034a4d3
evgen <parom.evgen@mail.ru>
2023-07-02 15:36:21 +0300

    first components + props

---
+++ project-01-starting/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link">Vue resource</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['id', 'title', 'description', 'link'],
+};
+</script>
