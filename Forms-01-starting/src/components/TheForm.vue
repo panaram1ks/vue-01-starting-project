db73e5723f0cf82a93cc803370d27e016c552ef4
evgen <parom.evgen@mail.ru>
2023-07-03 13:35:12 +0300

    custom Control Component

--- Forms-01-starting/src/components/TheForm.vue
+++ Forms-01-starting/src/components/TheForm.vue
@@ -99,6 +99,10 @@
       </div>
     </div>
     <div class="form-control">
+      <rating-control></rating-control>
+    </div>
+
+    <div class="form-control">
       <input
         type="checkbox"
         id="confirm-terms"
@@ -115,7 +119,10 @@
 </template>
 
 <script>
+import RatingControl from './RatingControl.vue';
+
 export default {
+  components: { RatingControl },
   data() {
     return {
       userName: '',
