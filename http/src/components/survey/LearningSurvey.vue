73d30c708504d707cc348009db3392560e8d198c
parom <parom.evgen@mail.ru>
2023-07-03 20:03:05 +0300

    Post request use fetch()

--- http/src/components/survey/LearningSurvey.vue
+++ http/src/components/survey/LearningSurvey.vue
@@ -13,22 +13,14 @@
           <label for="rating-poor">Poor</label>
         </div>
         <div class="form-control">
-          <input
-            type="radio"
-            id="rating-average"
-            value="average"
-            name="rating"
-            v-model="chosenRating"
-          />
+          <input type="radio" id="rating-average" value="average" name="rating" v-model="chosenRating" />
           <label for="rating-average">Average</label>
         </div>
         <div class="form-control">
           <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating" />
           <label for="rating-great">Great</label>
         </div>
-        <p
-          v-if="invalidInput"
-        >One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="invalidInput">One or more input fields are invalid. Please check your provided data.</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -46,7 +38,6 @@ export default {
       invalidInput: false,
     };
   },
-  emits: ['survey-submit'],
   methods: {
     submitSurvey() {
       if (this.enteredName === '' || !this.chosenRating) {
@@ -55,10 +46,17 @@ export default {
       }
       this.invalidInput = false;
 
-      this.$emit('survey-submit', {
-        userName: this.enteredName,
-        rating: this.chosenRating,
-      });
+      const myAddress = 'surveys.json'
+      fetch('https://vue-http-demo-e2a97-default-rtdb.europe-west1.firebasedatabase.app/' + myAddress, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: this.enteredName,
+          rating: this.chosenRating,
+        })
+      })
 
       this.enteredName = '';
       this.chosenRating = null;
