cb5dc2171b2c7338654674d869505b2736d07dcc
evgen <parom.evgen@mail.ru>
2023-07-08 15:48:37 +0300

    Modal animation, <transition> should has only one child inside

--- Animations-01/src/App.vue
+++ Animations-01/src/App.vue
@@ -13,10 +13,12 @@
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
-    <p>This is a test dialog!</p>
-    <button @click="hideDialog">Close it!</button>
-  </base-modal>
+  
+    <base-modal @close="hideDialog" :open="dialogIsVisible">
+      <p>This is a test dialog!</p>
+      <button @click="hideDialog">Close it!</button>
+    </base-modal>
+  
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
@@ -139,4 +141,6 @@ button:active {
   opacity: 0;
   transform: translateY(30px);
 } */
+
+
 </style>
--- Animations-01/src/components/BaseModal.vue
+++ Animations-01/src/components/BaseModal.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="backdrop" @click="$emit('close')"></div>
-  <dialog open>
-    <slot></slot>
-  </dialog>
+  <div v-if="open" class="backdrop" @click="$emit('close')"></div>
+  <transition name="modal">
+    <dialog open v-if="open">
+      <slot></slot>
+    </dialog>
+  </transition>
 </template>
 
 <script>
 export default {
+  props: ['open'],
   emits: ['close'],
 };
 </script>
@@ -35,7 +38,7 @@ dialog {
   z-index: 100;
   border: none;
 
-  animation: modal 0.5s ease-out forwards;
+  /* animation: modal 0.5s ease-out forwards; */
 }
 
 @keyframes modal {
@@ -47,5 +50,13 @@ dialog {
     opacity: 1;
     transform: translateY(0px) scale(1);
   }
+}
+
+/* Modal Window */
+.modal-enter-active {
+  animation: modal 0.3s ease-out;
+}
+.modal-leave-active {
+  animation: modal 0.3s ease-in reverse;
 }
 </style>
