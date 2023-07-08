b465f3c136fc3b4b2653b606afdb0ab1225abb41
evgen <parom.evgen@mail.ru>
2023-07-08 18:16:10 +0300

    prepare list users to make animation

--- Animations-01/src/App.vue
+++ Animations-01/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+    <users-list></users-list>
+  </div>
+  <div class="container">
     <div class="block" :class="{ animate: animatedBlock }"></div>
     <button @click="animateBlock">Animate</button>
   </div>
@@ -39,7 +42,10 @@
 </template>
 
 <script>
+import UsersList from './components/UsersList.vue';
+
 export default {
+  components: { UsersList },
   data() {
     return {
       animatedBlock: false,
