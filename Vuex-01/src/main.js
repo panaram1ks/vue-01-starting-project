f14086eae578ed6241b2f1a3e3abbc50cd664719
parom <E.Parominskiy@isida.by>
2023-07-13 15:26:22 +0300

    install and create Store!

--- Vuex-01/src/main.js
+++ Vuex-01/src/main.js
@@ -1,7 +1,25 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
 
+const store = createStore({
+    state() {
+        return {
+            counter: 0,
+        };
+    }
+});
+
 const app = createApp(App);
+app.use(store);
 
 app.mount('#app');
+
+
+/*
+npm install --vuex  or npm install --vuex@next if you need last version
+npm install --save vuex
+
+npm install vue-router
+*/
