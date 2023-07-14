2ac62431b62e779acc855e0ea236a3ff31b71ce5
parom <E.Parominskiy@isida.by>
2023-07-14 17:42:55 +0300

    create and register Router

--- CoachWebApp/src/main.js
+++ CoachWebApp/src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue';
+import app from './App.vue';
+import router from './router.js';
 
-createApp({}).mount('#app');
+
+const application = createApp(app)
+
+application.use(router)
+application.mount('#app');
+
 
 
 // npm install --save vue-router vuex
---
+++ CoachWebApp/src/router.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        { path: '/', redirect: 'coaches' },
+        { path: '/coaches', component: null },
+        {
+            path: '/coaches/:id', component: null, children: [
+                { path: '/contact', component: null },//  /coaches/c1/contact
+            ]
+        },
+        { path: '/register', component: null },
+        { path: '/requests', component: null },
+        { path: '/:notFound(.*)', component: null },
+    ],
+});
+
+export default router;
