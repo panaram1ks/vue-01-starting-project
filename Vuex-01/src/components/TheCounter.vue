4f9d65252bc759dcc1a17c74fd135779bcb0c80c
parom <E.Parominskiy@isida.by>
2023-07-14 08:26:41 +0300

    mapper Helpers

--- Vuex-01/src/components/TheCounter.vue
+++ Vuex-01/src/components/TheCounter.vue
@@ -1,13 +1,17 @@
 <template>
-    <h3>{{ counter }}</h3>
+    <!-- <h3>{{ counter }}</h3> -->
+    <h3>{{ finalCounter }}</h3>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-    computed: {
-        counter() {
-            return this.$store.getters.finalCounter          
-        }
-    }
+    // computed: {
+    //     counter() {
+    //         return this.$store.getters.finalCounter          
+    //     }
+    // }
+    ...mapGetters(['finalCounter']) // invoke getter from store
 }
 </script>
