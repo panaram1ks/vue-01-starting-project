01875ee53ced880421051de0d8c36595d8bcec93
parom <E.Parominskiy@isida.by>
2023-07-19 15:36:35 +0300

    Base components: Button, Card, Badge

--- CoachWebApp/src/main.js
+++ CoachWebApp/src/main.js
@@ -2,12 +2,19 @@ import { createApp } from 'vue';
 import app from './App.vue';
 import router from './router.js';
 import store from './store/index.js';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
 
 
 const application = createApp(app);
 
 application.use(router);
 application.use(store);
+
+application.component('base-card', BaseCard);
+application.component('base-button', BaseButton);
+application.component('base-badge', BaseBadge);
 
 application.mount('#app');
 
--- CoachWebApp/src/pages/coaches/CoachesList.vue
+++ CoachWebApp/src/pages/coaches/CoachesList.vue
@@ -3,18 +3,20 @@
         Filter
     </section>
     <section>
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
-        </div>
-        <ul v-if="hasCoaches">
-            <!-- <li v-for="coach in filteredCoaches" :key="coach.id">
+        <base-card>
+            <div class="controls">
+                <base-button mode="putline">Refresh</base-button>
+                <base-button link to="/register">Register as Coach</base-button>
+            </div>
+            <ul v-if="hasCoaches">
+                <!-- <li v-for="coach in filteredCoaches" :key="coach.id">
                 {{ coach.firstName }}
             </li> -->
-            <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
-                :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
-        </ul>
-        <h3 v-else>No coaches found</h3>
+                <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
+                    :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
+            </ul>
+            <h3 v-else>No coaches found</h3>
+        </base-card>
     </section>
 </template>
 
@@ -38,13 +40,13 @@ export default {
 
 <style scoped>
 ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
+    list-style: none;
+    margin: 0;
+    padding: 0;
 }
 
 .controls {
-  display: flex;
-  justify-content: space-between;
+    display: flex;
+    justify-content: space-between;
 }
 </style>
