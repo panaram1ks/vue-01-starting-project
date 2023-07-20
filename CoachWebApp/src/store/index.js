ae2e89288de169f6a4818b11a284dd914e49d373
parom <E.Parominskiy@isida.by>
2023-07-20 11:14:59 +0300

    adding Coaches to Vuex

--- CoachWebApp/src/store/index.js
+++ CoachWebApp/src/store/index.js
@@ -5,6 +5,16 @@ import coachModule from './modules/coaches/index.js';
 const store = createStore({
     modules: {
         coaches: coachModule
+    },
+    state() {
+        return {
+            userId: 'c3'
+        }
+    },
+    getters: {
+        userId(state) {
+            return state.userId;
+        }
     }
 });
 
