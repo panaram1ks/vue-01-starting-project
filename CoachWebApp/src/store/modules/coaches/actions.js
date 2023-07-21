1094ae6c62f20eddb9cfc6549e8368450c32eacb
parom <E.Parominskiy@isida.by>
2023-07-21 13:06:25 +0300

    put data to firebase!!

--- CoachWebApp/src/store/modules/coaches/actions.js
+++ CoachWebApp/src/store/modules/coaches/actions.js
@@ -1,14 +1,34 @@
 export default {
-    registerCoach(context, data) {
-        const coachData = {
-            // id: new Date().toISOString(),
-            id: context.rootGetters.userId,
-            firstName: data.first,
-            lastName: data.last,
-            description: data.desc,
-            hourlyRate: data.rate,
-            areas: data.areas,
-        }
-        context.commit('registerCoach', coachData)
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
+
+    const coachData = {
+      // id: new Date().toISOString(),
+      //   id: context.rootGetters.userId,
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas,
+    };
+
+    const response = await fetch(
+      `https://vue-http-demo-e2a97-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+    const responseData = await response.json();
+    console.log(responseData);
+
+    if (!response.ok) {
+      // handle Error ...
     }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
+  },
 };
