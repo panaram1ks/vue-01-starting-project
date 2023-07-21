830b3c8139a9afad38db5da30154391a56d55b3c
parom <E.Parominskiy@isida.by>
2023-07-21 14:39:46 +0300

    Get Http request, Fetching coach Data

--- CoachWebApp/src/pages/coaches/CoachesList.vue
+++ CoachWebApp/src/pages/coaches/CoachesList.vue
@@ -1,23 +1,32 @@
 <template>
-    <section>
-        <coach-filter @change-filter="setFilters"></coach-filter>
-    </section>
-    <section>
-        <base-card>
-            <div class="controls">
-                <base-button mode="putline">Refresh</base-button>
-                <base-button v-if="!isCoach" link to="/register">Register as Coach</base-button>
-            </div>
-            <ul v-if="hasCoaches">
-                <!-- <li v-for="coach in filteredCoaches" :key="coach.id">
+  <section>
+    <coach-filter @change-filter="setFilters"></coach-filter>
+  </section>
+  <section>
+    <base-card>
+      <div class="controls">
+        <base-button mode="putline" @click="loadCoaches">Refresh</base-button>
+        <base-button v-if="!isCoach" link to="/register"
+          >Register as Coach</base-button
+        >
+      </div>
+      <ul v-if="hasCoaches">
+        <!-- <li v-for="coach in filteredCoaches" :key="coach.id">
                 {{ coach.firstName }}
             </li> -->
-                <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
-                    :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
-            </ul>
-            <h3 v-else>No coaches found</h3>
-        </base-card>
-    </section>
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found</h3>
+    </base-card>
+  </section>
 </template>
 
 <script>
@@ -25,59 +34,65 @@ import CoachItem from '../../components/coaches/CoachItem.vue';
 import CoachFilter from '../../components/coaches/CoachFilter.vue';
 
 export default {
-    components: {
-        CoachItem,
-        CoachFilter,
-    },
-    computed: {
-        filteredCoaches() {
-            const coaches = this.$store.getters['coaches/coaches']; // first coaches is a namespace in index.js, second coachase is getter
-            return coaches.filter(coach => {
-                if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
-                    return true;
-                }
-                if (this.activeFilters.backend && coach.areas.includes('backend')) {
-                    return true;
-                }
-                if (this.activeFilters.career && coach.areas.includes('career')) {
-                    return true;
-                }
-                return false;
-            })
-        },
-        hasCoaches() {
-            return this.$store.getters['coaches/hasCoaches']
-        },
-        isCoach() {
-            return this.$store.getters['coaches/isCoach']
+  components: {
+    CoachItem,
+    CoachFilter,
+  },
+  computed: {
+    filteredCoaches() {
+      const coaches = this.$store.getters['coaches/coaches']; // first coaches is a namespace in index.js, second coachase is getter
+      return coaches.filter((coach) => {
+        if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+          return true;
         }
-    },
-    methods: {
-        setFilters(updatedFilters) {
-            this.activeFilters = updatedFilters;
+        if (this.activeFilters.backend && coach.areas.includes('backend')) {
+          return true;
         }
-    },
-    data() {
-        return {
-            activeFilters: {
-                frontend: true,
-                backend: true,
-                career: true,
-            }
+        if (this.activeFilters.career && coach.areas.includes('career')) {
+          return true;
         }
-    }
-}
+        return false;
+      });
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+    isCoach() {
+      return this.$store.getters['coaches/isCoach'];
+    },
+  },
+  created() {
+    this.loadCoaches();
+  },
+  methods: {
+    setFilters(updatedFilters) {
+      this.activeFilters = updatedFilters;
+    },
+    loadCoaches() {
+      this.$store.dispatch('coaches/loadCoaches');
+    },
+  },
+  data() {
+    return {
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
+  },
+};
 </script>
 
 <style scoped>
 ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
+  list-style: none;
+  margin: 0;
+  padding: 0;
 }
 
 .controls {
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
